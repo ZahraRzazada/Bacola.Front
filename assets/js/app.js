880a2a74a120d61e97c05fd931677b1fe3f0cef9
ZahraRzazada <zahraer@code.edu.az>
2024-02-20 10:38:22 +0400

    Half-part

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,73 @@
 // // -------------------Home-Slider-----------------
 $('.home-slider1').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          dots: false,
-          slidesToScroll: 1
-        }
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
       }
-    ]
-  });
-  $('.product-slider').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    arrows:true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          dots: false,
-          arrows:true,
-          slidesToScroll: 1
-        }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
-     
-    ]
-  });
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        dots: false,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+$('.product-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        dots: false,
+        arrows: true,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
 // --------------------------------------------
 
 //Shop Product----------------------------------
@@ -86,7 +86,7 @@ $('.addition-img').slick({
   speed: 500,
   asNavFor: '.main-img',
   dots: true,
-  arrows:false,
+  arrows: false,
   centerMode: true,
   focusOnSelect: true,
   // slide: 'div'
@@ -94,47 +94,58 @@ $('.addition-img').slick({
 //-----------------------------------------------
 
 //TabMneu--------------------------
-let tabbuttons=document.querySelectorAll('#Tabmenu .container .tabmenu .links button')
+let tabbuttons = document.querySelectorAll('#Tabmenu .container .tabmenu .links button')
 console.log(tabbuttons);
 console.log("salam");
-for(let tbtn of tabbuttons){
-tbtn.onclick=function(){
-let x=document.querySelector('.tabactive')
-x.classList.remove('tabactive')
-tbtn.classList.add('tabactive')
-let data_id=this.getAttribute('data-id')
-let items=document.querySelectorAll('#Tabmenu .container .tabmenu  .contents div')
-for( let div of items){
-if(div.getAttribute('id')===data_id){
-  div.classList.remove('d-none')
-}
-else{
-  div.classList.add('d-none')
-}
-}
-}
+for (let tbtn of tabbuttons) {
+  tbtn.onclick = function () {
+    let x = document.querySelector('.tabactive')
+    x.classList.remove('tabactive')
+    tbtn.classList.add('tabactive')
+    let data_id = this.getAttribute('data-id')
+    let items = document.querySelectorAll('#Tabmenu .container .tabmenu  .contents div')
+    for (let div of items) {
+      if (div.getAttribute('id') === data_id) {
+        div.classList.remove('d-none')
+      }
+      else {
+        div.classList.add('d-none')
+      }
+    }
+  }
 }
 //-------------------------------
 // Up button--------------------
-let up=document.getElementById('Up')
-window.onscroll=function(){
-  if(document.documentElement.scrollTop>200){
-up.style.opacity='1'
+let up = document.getElementById('Up')
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 200) {
+    up.style.opacity = '1'
   }
-  else{
-    up.style.opacity='0'
+  else {
+    up.style.opacity = '0'
   }
 }
 //---------------------------------
 
 
 //Category Dropdown----------------
-// var dbtn=document.querySelector("header .header-nav .container .all-categories .collapsed ");
+var dbtn = document.querySelector("header .header-nav .container .all-categories .collapsed ");
 // // console.log(dbtn);
 // var dropdown=document.querySelector("header .header-nav .container .all-categories .dropdown-category");
 // dropdown.classList
 
 
-  var dropdown=document.querySelector("header .header-nav .container .all-categories .dropdown-category");
-  console.log(dropdown);
-  // dropdown.classList.toggle("show");
+var dropdown = document.querySelector("header .header-nav .container .all-categories .dropdown-category");
+console.log(dropdown);
+// dropdown.classList.toggle("show");
+
+
+var dbtn = document.querySelector("header .header-nav .container .all-categories .collapsed ");
+function togglevisible(dbtn) {
+  var element = document.querySelector("header .header-nav .container .all-categories .dropdown-category");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
